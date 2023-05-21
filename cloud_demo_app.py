446f4b6376d11c59a741a4b2a801aff03de92980
Burak Ercan <bercan89@gmail.com>
2023-05-21 14:37:56 +0300

    use wide mode

--- cloud_demo_app.py
+++ cloud_demo_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(layout="wide")
 import streamlit.components.v1 as components
 from utils import get_eval_config_names, get_dataset_config_names, get_sequence_names
 from demo_urls import demo_urls
@@ -29,4 +30,4 @@ with col2:
 
 url = demo_urls[selected_dataset][selected_sequence] + "?loop=1&?autoplay=1&?hd=1&?nocontrols=1"
 html_string = "<div style=\"width: 100%; height: 0px; position: relative; padding-bottom: 84.341%;\"><iframe src=\"{}\" frameborder=\"0\" width=\"100%\" height=\"100%\" allowfullscreen style=\"width: 100%; height: 100%; position: absolute;\"></iframe></div>".format(url)
-components.html(html_string, height=600)
+components.html(html_string, height=800)
